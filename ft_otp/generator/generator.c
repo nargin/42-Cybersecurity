d14e19690c169d7dc9c1212f12d7451e39e44f21
nargin <mejumine@gmail.com>
2024-04-10 11:59:35 +0200

    Key generator in C

---
+++ ft_otp/generator/generator.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char HEX_CHAR[] = "0123456789ABCDEF";
+enum OutputDisplay {
+	FILE_FD, // Conflict with FILE macro
+	CONSOLE
+};
+
+char *generate_key(int size) {
+	char *key = malloc(size * sizeof(char) + 1);
+	for (int i = 0; i < size; i++) {
+		key[i] = HEX_CHAR[rand() % 16];
+	}
+	key[size] = 0;
+	return key;
+}
+
+int	file_output(int size) {
+	FILE *file = fopen("hex.key", "w");
+
+	if (file == NULL) {
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	
+	const char *key = generate_key(size);
+
+	fprintf(file, "%s", key);
+	fclose(file);
+	return 0;
+}
+
+int	console_output(int size) {
+	const char *key = generate_key(size);
+	printf("%s\n", key);
+	return 0;
+}
+
+void	argument_parser(int ac, char *av[], int *size, enum OutputDisplay *display) {
+	if (ac > 4) {
+		printf("Too many arguments\n");
+		exit(1);
+	}
+	for (int i = 1; i < ac; i++) {
+		if (av[i][0] == '-') {
+			switch (av[i][1]) {
+				case 'h':
+					printf("Usage: %s [-f file output] [-n size (default: 64)]\n", av[0]);
+					exit(0);
+				case 'n':
+					if (i + 1 >= ac) {
+						printf("Missing argument for -n\n");
+						exit(1);
+					}
+					*size = atoi(av[++i]);
+					break;
+				case 'f':
+					*display = FILE_FD;
+					break;
+				default:
+					printf("Invalid argument\n");
+					exit(1);
+			}
+		}
+	}
+}
+
+int	main(int ac, char *av[]) {
+	int size = 64;
+	enum OutputDisplay display = CONSOLE;
+
+	argument_parser(ac, av, &size, &display);
+	if (size <= 0) {
+		printf("Putting back size to 64 characters\n");
+		size = 64;
+	}
+	if (display == FILE_FD) {
+		file_output(size);
+	} else {
+		console_output(size);
+	}
+}
--- ft_otp/src/main.rs
+++ ft_otp/src/main.rs
@@ -3,6 +3,14 @@ use std::env;
 use std::io::{Write};
 use log::{error, debug};
 
+extern crate crypto;
+extern crate byteorder;
+
+use crypto::hmac::Hmac;
+use crypto::sha1::Sha1;
+use crypto::mac::Mac;
+use byteorder::{ByteOrder, BigEndian};
+
 
 const ARGS_ERROR: &str = "Usage: ./ft_otp [Option] [File]\n\
 Try './ft_otp --help or -h' for more information.";
@@ -32,7 +40,7 @@ fn	arguments_check(args: &Vec<String>) {
 	if args[1] == "-g" || args[1] == "--generate" {
 		let tested_string = String::from("Ohaio mina-san!~~");
 		let mut file = std::fs::File::create("test.tmp").expect("Unable to create file");
-		println!("File created at {}", std::fs::canonicalize("test.tmp").expect("Unable to get file path").display());
+		debug!("Test file created at {}", std::fs::canonicalize("test.tmp").expect("Unable to get file path").display());
 		file.write_all(tested_string.as_bytes()).expect("Unable to write data");
 		let contents = std::fs::read_to_string("test.tmp").expect("Unable to read file");
 		if contents != tested_string {
@@ -47,17 +55,18 @@ fn	arguments_check(args: &Vec<String>) {
 		hex_key = hex_key.trim().to_string();
 		println!("Key: {}", hex_key);
 		debug!("Hex key: {}", hex_key);
-		if hex_key.len() < 64 {
-			error!("Invalid key length: {}", hex_key.len());
-			println!("Expected 64 hexadecimal characters but got {}", hex_key.len());
+		if hex_key.len() != 64 || hex_key.chars().all(|c| c.is_ascii_hexdigit()) == false {
+			error!("Expected 64 hexadecimal characters: {}", hex_key.len());
 			std::process::exit(1);
 		}
 	}
 	if args[1] == "-k" || args[1] == "--key" {
 		let contents = std::fs::read_to_string("ft_otp.key").expect("Unable to read file");
 		debug!("File content: {}", contents);
+		let mut hex_key = contents.trim().to_string();
 	} 
 }
+
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
