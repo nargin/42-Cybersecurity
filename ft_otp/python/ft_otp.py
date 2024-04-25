bcdfab7402e9a2498204062e38b44dbef0458a0b
nargin <mejumine@gmail.com>
2024-04-25 23:06:45 +0200

    TOTP Done with 🐍

--- ft_otp/python/ft_otp.py
+++ ft_otp/python/ft_otp.py
@@ -7,35 +7,44 @@
 import argparse
 import base64
 import pyotp
+import time
+import os
 
-    # base32_key = base64.b32encode(bytes.fromhex(key))
-    # print(base32_key)
-    # hotp = pyotp.HOTP(base32_key)
-    # print(hotp.at(0))
+def generate_password(key: str):
+	if len(key) < 64:
+		exit("Key must be at least 64 characters long")
+	if not all(c in '0123456789abcdefABCDEF' for c in key):
+		exit("Key must be hexadecimal")
+	base32_key = base64.b32encode(bytes.fromhex(key))
+	with open('ft_otp.key', 'wb') as f:
+		f.write(base32_key)
+	print("Password generated in ft_otp.key")
 
-def generate_password(key):
-    if len(key) < 64:
-        exit("Key must be at least 64 characters long")
-    if not all(c in '0123456789abcdefABCDEF' for c in key):
-        exit("Key must be hexadecimal")
-    base32_key = base64.b32encode(bytes.fromhex(key))
-    with open('ft_otp.key', 'wb') as f:
-        f.write(base32_key)
+def generate_key():
+	if not os.path.exists('ft_otp.key'):
+		exit("No key found")
+	with open('ft_otp.key', 'rb') as f:
+		base32_key = f.read()
+		hotp = pyotp.HOTP(base32_key)
+		timestamp = int(time.time() / 30)
+		totp = hotp.at(timestamp)
+		print(totp)
+
+def in_venv():
+	if os.getenv('VIRTUAL_ENV') is None:
+		exit('Please run the script in a virtual environment')
 
 def main():
-    parser = argparse.ArgumentParser(description='OTP generator')
-    parser.add_argument('-g', '--generate', type=str, help='Generate a new key')
-    parser.add_argument('-k', '--key', type=str, help='Generate a new password based on the key')
-    args = parser.parse_args()
+	in_venv()
+	parser = argparse.ArgumentParser(description='OTP generator')
+	parser.add_argument('-g', '--generate', type=str, help='Generate a new key')
+	parser.add_argument('-k', '--key', help='Generate a new password based on the key', action='store_true')
+	args = parser.parse_args()
 
-    print(args)
-
-    # if args.generate:
-    #     generate_key(args.generate)
-    # elif args.key:
-    generate_password(args.key)
-    # else:
-        # parser.print_help()
+	if args.generate:
+	    generate_password(args.generate)
+	elif args.key:
+		generate_key()
 
 if __name__ == '__main__':
-    main()
+	main()
---
+++ ft_otp/python/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./gen -f # -n 128
+
+random_key=$(cat hex.key)
+
+python3 ft_otp.py -g $random_key
+
+my_totp=$(python3 ft_otp.py -k)
+echo "My TOTP: $my_totp"
+
+oauth_totp=$(oathtool --totp $random_key)
+echo "OAuth TOTP: $oauth_totp"
+
+rm hex.key ft_otp.key
